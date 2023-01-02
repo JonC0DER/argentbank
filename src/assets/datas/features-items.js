2c01617360dadea3a9544366605cf2f6998f58dc
joncoder <joncoder.kamite@gmail.com>
2023-01-02 22:25:07 +0100

    creation de données json

---
+++ src/assets/datas/features-items.js
@@ -0,0 +1,19 @@
+const featuresItems = [
+    {
+        img: require("../images/icon-chat.png"),
+        title: "You are our #1 priority",
+        text: "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
+    },
+    {
+        img: require("../images/icon-money.png"),
+        title: "More savings means higher rates",
+        text: "The more you save with us, the higher your interest rate will be!"
+    },
+    {
+        img: require("../images/icon-security.png"),
+        title: "Security you can trust",
+        text: "We use top of the line encryption to make sure your data and money is always safe."
+    }
+];
+
+export default featuresItems;
