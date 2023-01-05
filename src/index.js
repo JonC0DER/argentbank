b709f29115af1f6e5d75d38f429ccb2bfb471998
joncoder <joncoder.kamite@gmail.com>
2023-01-05 14:24:31 +0100

    modification du fichier index avec ajout du store & provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './css/main.css';
+import App from '../src/component/App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import {store} from './redux_components/store';
 
+//const storeUpdate = JSON.stringify(store.subscribe(()=> console.log(store.getState())))
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+root.render( 
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
