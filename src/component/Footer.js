20cbaa4040dddfc3e872dfe16637f420a5883265
joncoder <joncoder.kamite@gmail.com>
2023-01-05 14:22:38 +0100

    creation du footer

---
+++ src/component/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+  render() {
+    return (
+        <footer className="footer">
+            <p className="footer-text">Copyright 2020 Argent Bank</p>
+        </footer>
+    )
+  }
+}
+
+export default Footer
