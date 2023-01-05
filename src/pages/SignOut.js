e86b694da338463f78926d32ba32c617362d0039
joncoder <joncoder.kamite@gmail.com>
2023-01-05 14:40:46 +0100

    creation du composant. vérifie si les state sont bien réinitialisé et redirige vers la page d'accueil

--- src/pages/SignOut.js
+++ src/pages/SignOut.js
@@ -1,39 +1,17 @@
-import { useDispatch, useSelector } from "react-redux"
+import { useSelector } from "react-redux"
 import { Navigate } from "react-router-dom"
-import { logOut, selectUserToken } from "../redux_components/Reducer"
-import ReactLoading from 'react-loading'
+import { selectUserToken } from "../redux_components/Reducer"
 import { Suspense } from "react"
+import Loading from "../component/Loading"
 
 const SignOut = () => {
-    const dispatch = useDispatch()
     
-    const onLogOut = () => dispatch( logOut() )
-    //const onLogOutTest = () => console.log('logout test')
+    const token = useSelector(selectUserToken)
+    console.log(token)
 
-    const Loading = () => (
-        <div>
-            <h1>Log Out...</h1>
-            <div onLoad={onLogOut}>
-                <ReactLoading 
-                    type="bars"
-                    color="LightGreen"
-                    height={800}
-                    width={200}
-                />
-            </div>
-        </div>
-    )
-
-    const ManageLogOut = () => {
-        const token = useSelector(selectUserToken)
-        return token
-            ? <Loading />
-            : <Navigate to='/' />
-    }
- 
     return (
-        <Suspense fallback={<Loading/>}>
-            <ManageLogOut/>
+        <Suspense fallback={<Loading msg={'Log Out'}/>}>
+            {!token && <Navigate to='/' />}
         </Suspense>
     )
 }
