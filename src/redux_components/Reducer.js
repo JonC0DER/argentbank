c86db37008c456a5aaf4517fd034aaecaf29634a
joncoder <joncoder.kamite@gmail.com>
2023-01-05 14:39:11 +0100

    modification du Reducer en refactorisant la fonction logOut(state)=> initialState

--- src/redux_components/Reducer.js
+++ src/redux_components/Reducer.js
@@ -5,7 +5,8 @@ const initialState = {
     email: '',
     password: '',
     status: 'idle',
-    token: null
+    token: null,
+    profile: {}
 }
 
 const loginSlice = createSlice({
@@ -13,19 +14,14 @@ const loginSlice = createSlice({
     initialState,
     reducers: {
         loginAdd: {
-            reducer(state, action){
+            reducer(state, action) {
                 state.email = action.payload.email
                 state.password = action.payload.password
             }
         },
-        logOut: {
-            reducer(state){
-                console.log('on log out reducer')
-                state.login = initialState
-            }
-        }
+        logOut: state => initialState
     },
-    extraReducers(builder){
+    extraReducers(builder) {
         builder
             .addCase(fetchLoginToken.pending, (state) => {
                 state.status = 'pending'
@@ -38,15 +34,27 @@ const loginSlice = createSlice({
                 state.status = 'failed'
                 state.error = action.error.message;
             })
+            .addCase(loadUserAccount.pending, (state) => {
+                state.status = "pending"
+                //state.addProfile.status = "pending"
+            })
             .addCase(loadUserAccount.fulfilled, (state, action) => {
+                //state.addProfile.status = "succeeded"
+                state.status = "succeeded"
                 state.profile = action.payload
+            })
+            .addCase(loadUserAccount.rejected, (state, action) => {
+                state.status = 'failed'
+                //state.addProfile.status = "failed"
+                state.error = action.error.message;
             })
     }
 })
 
 export const selectUserLogin = (state) => state.login
 export const selectUserToken = (state) => state.login.token
-export const getUserProfile = (state) => state.profile
+export const getUserProfile = (state) => state.login.profile
+export const getUserProfileStatus = (state) => state.login.status
 
 export const { loginAdd, logOut } = loginSlice.actions
 
