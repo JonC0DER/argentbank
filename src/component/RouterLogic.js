82cf4af1e66d7dbb6986df69724ffea9291003b4
joncoder <joncoder.kamite@gmail.com>
2023-01-07 22:25:57 +0100

    renommage des composants de routage pour une meilleur compréhension

--- src/component/RouterLogic.js
+++ src/component/RouterLogic.js
@@ -3,22 +3,26 @@ import Home from '../pages/Home'
 import NavBar from './NavBar'
 import OnSignOut from '../component/services/OnSignOut'
 import LoadSignInOrUserProfile from './services/LoadSignInOrUserProfile'
+import SignUp from '../pages/SignUp'
+import UserProfile from '../pages/UserProfile'
 
 const RouterLogic = () => {
   return (
-      <BrowserRouter>  
-          <NavBar/>
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/sign-in" element={
-              <LoadSignInOrUserProfile />
-            } />
-            <Route path="/user-account" element={
-              <LoadSignInOrUserProfile />
-            } />
-            <Route path='/sign-out' element={<OnSignOut/>} />
-          </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={
+          <LoadSignInOrUserProfile />
+        } />
+        <Route path="/user-account" element={
+          <LoadSignInOrUserProfile />
+        } />
+        <Route path='/user-profile' element={<UserProfile />} />
+        <Route path='/sign-out' element={<OnSignOut />} />
+        <Route path='/sign-up' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
