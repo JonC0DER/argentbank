8d2fead0624e4a8ee59b82ae97ff1cbceee5e062
joncoder <joncoder.kamite@gmail.com>
2023-01-07 22:11:32 +0100

    renommage des composants loggedIn => loggedInNav et SignIn => SignInNav

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,19 +1,21 @@
 import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
-import { selectUserToken } from '../redux_components/Reducer'
+import { getUserProfileName, selectUserToken } from '../redux_components/Reducer'
 
 export const NavBar = () => {
 
   const userToken = useSelector(selectUserToken)
+  const userName = useSelector(getUserProfileName)
+  const capitalize = (word) => word.charAt(0).toUpperCase() + word.slice(1)
 
   const LoggedInNav = () => (
     <>
       <NavLink className="main-nav-item" to="/user-account">
-        <i className="fa fa-user-circle"></i>
-        Tony
+        <i className="fa fa-user-circle"></i>&nbsp;
+        {userName ? capitalize(userName) : "UserName"}
       </NavLink>
       <NavLink className="main-nav-item" to="/sign-out">
-        <i className="fa fa-sign-out"></i>
+        <i className="fa fa-sign-out"></i>&nbsp;
         Sign Out
       </NavLink>
     </>
@@ -21,8 +23,12 @@ export const NavBar = () => {
 
   const SignInNav = () => (
     <>
+      <NavLink className="main-nav-item" to="/sign-up">
+        <i className="fa fa-file-text"></i>&nbsp;
+        Sign Up
+      </NavLink>
       <NavLink className="main-nav-item" to="/sign-in">
-        <i className="fa fa-user-circle"></i>
+        <i className="fa fa-sign-in"></i>&nbsp;
         Sign In
       </NavLink>
     </>
@@ -37,8 +43,8 @@ export const NavBar = () => {
         />
         <h1 className="sr-only">Argent Bank</h1>
       </NavLink>
-      <div> 
-        {!userToken ? <SignInNav/>: <LoggedInNav/>}
+      <div>
+        {!userToken ? <SignInNav /> : <LoggedInNav />}
       </div>
     </nav>
   )
